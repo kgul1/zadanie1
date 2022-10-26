fc2b108c9d150e09e341d98c01fa4546d8a04233
kgul1 <113103268+kgul1@users.noreply.github.com>
2022-10-26 16:10:04 +0200

    Add files via upload

---
+++ zadanie.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main(void)
+{
+    float a, h, pole;
+    cout << "podaj liczbe a: ";
+    cin >> a;
+    cout << "podaj liczbe b: ";
+    cin >> h;
+    pole = a * h / 2;
+    cout << "wynik wynosi: " << pole;
+
+    return 0;
+}
